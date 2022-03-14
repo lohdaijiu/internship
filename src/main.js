775b82c0abed5a87b59c299e4bd15903a2a749ed
Roopa Niedu <bitznobel@gmail.com>
2022-03-15 01:47:45 +0800

    Installed element plus and added to main

--- src/main.js
+++ src/main.js
@@ -1,22 +1,23 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from "./router/index.js";
-import { initializeApp } from 'firebase/app'
-
-
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router/index.js';
+import { initializeApp } from 'firebase/app';
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB9kuUTDuvy3-S49MPlF22y9NNvFrtDLJg",
-    authDomain: "internship-433c3.firebaseapp.com",
-    projectId: "internship-433c3",
-    storageBucket: "internship-433c3.appspot.com",
-    messagingSenderId: "674179049290",
-    appId: "1:674179049290:web:efa98ea8256771509603b3",
-    measurementId: "G-X3EZ6VJ8LX"
+  apiKey: 'AIzaSyB9kuUTDuvy3-S49MPlF22y9NNvFrtDLJg',
+  authDomain: 'internship-433c3.firebaseapp.com',
+  projectId: 'internship-433c3',
+  storageBucket: 'internship-433c3.appspot.com',
+  messagingSenderId: '674179049290',
+  appId: '1:674179049290:web:efa98ea8256771509603b3',
+  measurementId: 'G-X3EZ6VJ8LX'
 };
 
 initializeApp(firebaseConfig);
 
 const app = createApp(App);
 app.use(router);
-app.mount("#app");
+app.use(ElementPlus);
+app.mount('#app');
