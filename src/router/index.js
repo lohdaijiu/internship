4bcab576de5060fe20aa0f9e79fa7f6f41d217a5
lohdaijiu <lohdaijiu@gmail.com>
2022-03-14 21:29:17 +0800

    Refactor and fix bugs

--- src/router/index.js
+++ src/router/index.js
@@ -61,7 +61,7 @@ const router = createRouter({
       component: LandingPage
     },
     {   
-      path: '/StudentProfile',
+      path: '/studentprofile',
       name: 'StudentProfile',
       component: StudentProfile,
   },
