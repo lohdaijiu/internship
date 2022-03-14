abcfe4864dfa042328feae1a1cfb2aa06e004ad9
yjlim2022 <yj_lim2020@outlook.com>
2022-03-14 15:54:01 +0800

    updated StudentProfile.vue in views to work with index.js

---
+++ src/views/StudentProfile.vue
@@ -0,0 +1,16 @@
+<template>
+    <StudentNav/>
+    <StudentProfile/>
+</template>
+
+<script>
+import StudentNav from '@/components/StudentNav.vue'
+import StudentProfile from '@/components/StudentProfile.vue'
+export default {
+    name: 'App',
+    components: {
+        StudentNav,
+        StudentProfile
+    }
+}
+</script>
