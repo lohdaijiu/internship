4043a9468a63105733d1cc60298e5bbaf623beda
yjlim2022 <yj_lim2020@outlook.com>
2022-03-16 15:58:48 +0800

    removed global styling for scrollbar and html page

--- src/components/StudentNav.vue
+++ src/components/StudentNav.vue
@@ -17,34 +17,8 @@
 
 
 </script>
-<style>
-::-webkit-scrollbar {
-  width: 10px;
-}
 
-/* Track */
-::-webkit-scrollbar-track {
-  background: #f1f1f1;
-}
 
-/* Handle */
-::-webkit-scrollbar-thumb {
-  background: #888;
-  border-radius: 15px;
-}
-
-/* Handle on hover */
-::-webkit-scrollbar-thumb:hover {
-  background: #555;
-}
-body, html {
-  padding: 0;
-  margin: 0;
-  width: 100%;
-  min-height: 100vh;
-  background-color: rgb(230, 240, 248) ;
-}
-</style>
 
 <style scoped>
 #signButton {
