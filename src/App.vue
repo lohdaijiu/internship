c452d0e4ad3b54848bb413acc6898fd8bd42f166
yjlim2022 <yj_lim2020@outlook.com>
2022-03-16 15:41:01 +0800

    removed styling from App.vue

--- src/App.vue
+++ src/App.vue
@@ -16,42 +16,5 @@ export default {
 </script>
 
 <style>
-@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap");
 
-#app {
-
-  font-family: 'Poppins', sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-
-}
-::-webkit-scrollbar {
-  width: 10px;
-}
-
-/* Track */
-::-webkit-scrollbar-track {
-  background: #f1f1f1;
-}
-
-/* Handle */
-::-webkit-scrollbar-thumb {
-  background: #888;
-  border-radius: 15px;
-}
-
-/* Handle on hover */
-::-webkit-scrollbar-thumb:hover {
-  background: #555;
-}
-
-body, html {
-  padding: 0;
-  margin: 0;
-  width: 100%;
-  min-height: 100vh;
-  background-color: rgb(230, 240, 248);
-}
 </style>
