bbc65fb2e4ec550cde4841e5ad728566af648e9d
yjlim2022 <yj_lim2020@outlook.com>
2022-03-16 14:34:35 +0800

    Deployed Edit student profile page with StudentNav  and EditStudentProfile components.

---
+++ src/views/Student/EditStudentProfile.vue
@@ -0,0 +1,16 @@
+<template>
+    <StudentNav/>
+    <EditStudentProfile/>
+</template>
+
+<script>
+import EditStudentProfile from '@/components/EditStudentProfile.vue'
+import StudentNav from '@/components/StudentNav.vue'
+export default {
+    name: 'App',
+    components: {
+        EditStudentProfile,
+        StudentNav
+    }
+}
+</script>
