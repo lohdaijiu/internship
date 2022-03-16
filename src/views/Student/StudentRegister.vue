87ef87a7e74ba8340e110606779b58de3e2f2594
Roopa Niedu <bitznobel@gmail.com>
2022-03-16 17:53:37 +0800

    Designed Student Registration UI

--- src/views/Student/StudentRegister.vue
+++ src/views/Student/StudentRegister.vue
@@ -1,82 +1,229 @@
-
+// 96C67F
 <template>
-  <h1>Create an Account for Students</h1>
-  Full name 
-  <p><input type="text"  v-model="name"/></p>
-    Place of Study
-  <p><input type="text" placeholder="" v-model="pos"/></p>
-    Major
-    <p><input type="text" placeholder="Email" v-model="major"/></p>
-    Email Address
-  <p><input type="email" placeholder="Email" v-model="email"/></p>
-  Password
-  <p><input type="password" placeholder="Password" v-model="password"/></p>
-  Confirm Password
-    <p><input type="password" placeholder="Password" v-model="password1"  /></p>
-  <p><button v-on:click="register()">Submit</button></p>
+  <el-container>
+    <el-header>
+      <el-row class="header-row">
+        <el-col :span="4" class><div class="grid-content bg-purple" /></el-col>
+        <el-col :span="16" class
+          ><p class="header-brand">
+            Take a step towards your future career
+          </p></el-col
+        >
+        <el-col :span="3" class
+          ><el-button
+            @click="redirectToStudentLogin()"
+            class="login-btns"
+            size="large"
+            color="#96C67F"
+            value="Students"
+            ><p class="btn-text">Log In</p></el-button
+          ></el-col
+        >
+        <el-col :span="1" class></el-col>
+      </el-row>
+    </el-header>
+    <el-main>
+      <el-row>
+        <el-col :span="24"><div class="grid-content" /></el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="7"></el-col>
+        <el-col :span="10">
+          <el-form :model="form" label-width="160px">
+            <el-form-item label="Full Name" class="input-labels">
+              <el-input v-model="form.name" />
+            </el-form-item>
+            <el-form-item label="Place of Study" class="input-labels">
+              <el-input v-model="form.pos" />
+            </el-form-item>
+            <el-form-item label="Major" class="input-labels">
+              <el-input v-model="form.major" />
+            </el-form-item>
+            <el-form-item label="Email Address" class="input-labels">
+              <el-input v-model="form.email" />
+            </el-form-item>
+            <el-form-item label="Password" class="input-labels">
+              <el-input v-model="form.password" />
+            </el-form-item>
+            <el-form-item label="Confirm Password" class="input-labels">
+              <el-input v-model="form.password1" />
+            </el-form-item>
+            <el-row>
+              <el-col :span="8"></el-col>
+              <el-col :span="8">
+                <el-button
+                  @click="register()"
+                  class="register-btns"
+                  size="large"
+                  color="#96C67F"
+                  ><p class="btn-text">Register</p></el-button
+                >
+              </el-col>
+              <el-col :span="8"></el-col>
+            </el-row>
+          </el-form>
+        </el-col>
+        <el-col :span="7"></el-col>
+      </el-row>
+    </el-main>
+  </el-container>
 </template>
 
 <script>
-import {getAuth, createUserWithEmailAndPassword} from "firebase/auth"
-import firebaseApp from '../../main.js'
-import { getFirestore } from "firebase/firestore"
-import {doc, setDoc } from "firebase/firestore"
-
-
-
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import firebaseApp from "../../main.js";
+import { getFirestore } from "firebase/firestore";
+import { doc, setDoc } from "firebase/firestore";
+import { reactive } from "vue";
 export default {
-    data() {
-        return {
-            email: "",
-            password: "",
-            password1: "",
-            error: "",
-            major: "",
-            name: "",
-            pos: "",
-            photoURL: "https://firebasestorage.googleapis.com/v0/b/internship-433c3.appspot.com/o/profile%2Fblank-pic.jpg?alt=media&token=730520eb-c8c6-4345-83d5-807015471610",
-            profileData: [{"About" : ""}, {"Work Experience" : ""}, {"Skills and Competencies" : ""}, {"Career Interests" : ""}, {"Projects" : ""}]
-        };
+  data() {
+    return {
+      form: reactive({
+        name: "",
+        pos: "",
+        major: "",
+        email: "",
+        password: "",
+        password1: "",
+      }),
+      email: "",
+      password: "",
+      password1: "",
+      error: "",
+      major: "",
+      name: "",
+      pos: "",
+      photoURL:
+        "https://firebasestorage.googleapis.com/v0/b/internship-433c3.appspot.com/o/profile%2Fblank-pic.jpg?alt=media&token=730520eb-c8c6-4345-83d5-807015471610",
+      profileData: [
+        { About: "" },
+        { "Work Experience": "" },
+        { "Skills and Competencies": "" },
+        { "Career Interests": "" },
+        { Projects: "" },
+      ],
+    };
+  },
+  methods: {
+    redirectToStudentLogin() {
+      this.$router.push({ path: "/studentlogin" });
     },
-    methods: {
-        async register() {
-            if (this.password != this.password1) {
-              alert("Password is different")
-              return;
-            } 
+    async register() {
+      if (this.form.password != this.form.password1) {
+        alert("Password is different");
+        return;
+      }
 
-            if (this.email == "" || this.password == "" || this.password1 == "" || this.major == "" || this.name == "" || this.pos == "") {
-              alert("Please fill in all the blanks");
-              return;
-            }
+      if (
+        this.form.email == "" ||
+        this.form.password == "" ||
+        this.form.password1 == "" ||
+        this.form.major == "" ||
+        this.form.name == "" ||
+        this.form.pos == ""
+      ) {
+        alert("Please fill in all the blanks");
+        return;
+      }
 
-            const auth = getAuth();
-            const db = getFirestore(firebaseApp);
-            
-            createUserWithEmailAndPassword(auth, this.email, this.password)
-            .then(() => {const uid = auth.currentUser.uid;
-            try {
-              const docRef = setDoc(doc(db, "User", uid), {
-                Email: this.email, 
-                Major: this.major, 
-                Name: this.name, 
-                PlaceOfStudy: this.pos, 
-                photoURL: this.photoURL, 
-                ProfileData: this.profileData, 
-                Employer: false
-            })
-            console.log(docRef)
-            } catch (error) {
-              console.error(error);
-            }})
-            .catch((error) => {alert(error.message)} )
+      const auth = getAuth();
+      const db = getFirestore(firebaseApp);
 
+      createUserWithEmailAndPassword(auth, this.form.email, this.form.password)
+        .then(() => {
+          const uid = auth.currentUser.uid;
+          try {
+            const docRef = setDoc(doc(db, "User", uid), {
+              Email: this.form.email,
+              Major: this.form.major,
+              Name: this.form.name,
+              PlaceOfStudy: this.form.pos,
+              photoURL: this.photoURL,
+              ProfileData: this.profileData,
+              Employer: false,
+            });
+            console.log(docRef);
+          } catch (error) {
+            console.error(error);
+          }
+        })
+        .catch((error) => {
+          alert(error.message);
+        });
 
-            
-
-            this.$router.push("/studentlogin");
-        },
-    }
-}
-
+      this.$router.push("/studentlogin");
+    },
+  },
+};
 </script>
+
+<style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap");
+
+.btn-text {
+  color: #1f1d2a;
+  font-weight: 600;
+}
+.login-btns {
+  width: 100%;
+  border: 1px solid black;
+}
+.register-btns {
+  margin-top: 20px;
+  width: 100%;
+}
+.header-row {
+  margin-top: 20px;
+}
+.header-brand {
+  font-family: "Poppins", sans-serif;
+  font-weight: 500;
+  color: #1f1d2a;
+  margin: 0;
+  font-size: 28px;
+  text-align: center;
+}
+.el-header {
+  background-color: #96c67f;
+  height: 80px;
+}
+.el-row {
+  /* margin-top: 20px; */
+}
+.el-row:last-child {
+  margin-bottom: 0;
+}
+.el-col {
+  border-radius: 4px;
+}
+.bg-purple-dark {
+  background: #99a9bf;
+}
+.bg-purple {
+  background: #d3dce6;
+}
+.bg-purple-light {
+  background: #e5e9f2;
+}
+/* .grid-content {
+  border-radius: 4px;
+  min-height: 36px;
+} */
+.row-bg {
+  padding: 10px 0;
+  background-color: #f9fafc;
+}
+body {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  background-color: #fafafa;
+}
+</style>
+<style>
+.input-labels .el-form-item__label {
+  font-family: "Poppins", sans-serif;
+  font-size: 15px;
+  font-weight: 500;
+}
+</style>
