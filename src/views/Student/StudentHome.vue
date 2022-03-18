7d3aa4283440e6f085b83141e20cfafc08d50de9
kk-hue <70795164+kk-hue@users.noreply.github.com>
2022-03-18 23:29:31 +0800

    Update StudentHome.vue

--- src/views/Student/StudentHome.vue
+++ src/views/Student/StudentHome.vue
@@ -1,19 +1,42 @@
 <template>
       <StudentNav/>
-
+      <div v-if="user">
+          <div>
+              <p> Welcome! Jason <br/>
+          <div>
+      </div>
 </template>
 
 <script>
 import StudentNav from '../../components/StudentNav.vue'
+
+const StudentData = [
+        numOfRej: '3',
+        ApplicationInProgress: '11',
+        ApplicationComp: '14'];
+
 export default {
     name: 'StudentHome',
-
     components:{
         StudentNav
-    }
 
+    },
     
-
+    data() {
+        return {
+            StudentData,
+            jobTypeData:{'Software Engineer': 35%, 'Data Analyst': 65%},
+            companyDistributionData:{'Grab': 25%, 'Shopee':25%, 'ByteDance':50%}
+        }
+    },
+    mounted() {
+        const auth = getAuth();
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+                this.user = user;
+            }
+        })
+    },
 }
 </script>
 
