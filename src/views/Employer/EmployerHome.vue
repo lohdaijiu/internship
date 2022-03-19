41b45d1f6fb0b92b33db7eb71ff7f6eea9d9fdb7
kk-hue <70795164+kk-hue@users.noreply.github.com>
2022-03-19 10:20:36 +0800

    Update EmployerHome.vue

--- src/views/Employer/EmployerHome.vue
+++ src/views/Employer/EmployerHome.vue
@@ -1,34 +1,16 @@
 !<template>
   <EmployerNav/>
-
-  <Welcome msg = "Welcome! DATASPARK PTE.LTD"/>
-
-  <h2>Analytics</h2>
-
+  
 </template>
 
 <script>
 import EmployerNav from "../../components/EmployerNav.vue"
-const EmployerData = [numOfRoles: '5'];
 
 export default {
     name: 'EmployerHome',
 
-    data() {
-        return {
-            EmployerData,
-            jobTypeData:{'Data Analyst': 5, 'Data Engineer': 7, 'Software Engineer': 10, 
-            'Data Scientist': 8, 'Algorithm Engineer':10}
-        }
-    },
-
     mounted() {
-        const auth = getAuth();
-        onAuthStateChanged(auth, (user) => {
-            if (user) {
-                this.user = user;
-            }
-        })
+       
     },
 
     methods: {
@@ -37,16 +19,10 @@ export default {
 
     components: {
       EmployerNav,
-      Welcome
     }
 }
 </script>
 
 <style>
-#Welcome{
-  text-align: left;
-  color: black;
-  text-size-adjust: 10px;
-}
 
 </style>
