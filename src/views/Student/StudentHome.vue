39aa9aeff833ad8c7595f4b2c71bd25e9246983d
kk-hue <70795164+kk-hue@users.noreply.github.com>
2022-03-23 20:33:08 +0800

    Add files via upload

---
+++ src/views/Student/StudentHome.vue
@@ -0,0 +1,26 @@
+<template>
+      <StudentNav/>
+      <StudentHome/>
+</template>
+
+<script>
+import StudentNav from '../../components/StudentNav.vue'
+import StudentHome from '@/components/StudentHome.vue'
+
+export default {
+    name: 'App',
+    components:{
+        StudentNav,
+        StudentHome
+    },
+    
+   
+    mounted() {
+     
+    },
+}
+</script>
+
+<style>
+
+</style>
