6c17b4b06a109b0929ca9c5f2391afd77f5ff92f
Roopa Niedu <bitznobel@gmail.com>
2022-03-24 12:44:09 +0800

    Enable display of searched job listings in table

--- src/views/Student/StudentJobBoard.vue
+++ src/views/Student/StudentJobBoard.vue
@@ -35,13 +35,12 @@
     <el-col :span="2"></el-col>
   </el-row>
 
-  
   <el-row>
     <el-col :span="2"></el-col>
     <el-col :span="20">
       <el-table
         ref="tableRef"
-        :data="jobData"
+        :data="searchResult"
         :default-sort="{ prop: 'postdate', order: descending }"
         height="250"
         style="width: 100%"
@@ -50,29 +49,14 @@
         <el-table-column prop="jobpos" label="Job Position" width="180" />
         <el-table-column prop="postdate" label="Date Posted" sortable />
         <el-table-column prop="duration" label="Work Duration" />
-        <el-table-column
-          prop="yos"
-          label="Year of Study"
-          :filters="[
-            { text: '1', value: '1' },
-            { text: '2', value: '2' },
-            { text: '3', value: '3' },
-            { text: '4', value: '4' },
-          ]"
-          :filter-method="filterTag"
-          filter-placement="bottom-end"
-        >
-        </el-table-column>
+        <el-table-column prop="yos" label="Year of Study"> </el-table-column>
         <el-table-column prop="range" label="Date Range" />
 
-        <el-table-column >
+        <el-table-column>
           <template #default="scope">
-             <el-button
-          size="small"
-          type="success"
-          @click="apply(scope.row)"
-          >Apply</el-button
-        >
+            <el-button size="small" type="success" @click="apply(scope.row)"
+              >Apply</el-button
+            >
           </template>
         </el-table-column>
       </el-table>
@@ -91,19 +75,26 @@
 // import { computed, ref } from "vue";
 import StudentNav from "../../components/StudentNav.vue";
 import { Search } from "@element-plus/icons-vue";
-import { getDocs, setDoc, collection, getDoc, getFirestore, updateDoc, arrayUnion, doc, serverTimestamp } from "firebase/firestore";
+import {
+  getDocs,
+  setDoc,
+  collection,
+  getDoc,
+  getFirestore,
+  updateDoc,
+  arrayUnion,
+  doc,
+  serverTimestamp,
+} from "firebase/firestore";
 import firebaseApp from "../../main.js";
 import { getAuth } from "firebase/auth";
 
 //   // dummy values for frontend
 
 var jobData = [];
-  
 
 export default {
   name: "StudentJobBoard",
-
-    
 
   components: {
     StudentNav,
@@ -117,7 +108,15 @@ export default {
   computed: {
     searchResult() {
       const { jobData, keyword } = this;
-      return jobData.filter(({ companyname }) => companyname.includes(keyword));
+      console.log(jobData, keyword);
+      console.log(
+        jobData.filter(({ companyname }) =>
+          companyname.toLowerCase().includes(keyword.toLowerCase())
+        )
+      );
+      return jobData.filter(({ companyname }) =>
+        companyname.toLowerCase().includes(keyword.toLowerCase())
+      );
     },
   },
 
@@ -134,90 +133,101 @@ export default {
     //     return docSnap.data().Name;
     //   } catch {
     //     console.log("error")
-    //   }  
+    //   }
     // },
     async apply(x) {
+      console.log(x);
       const db = getFirestore(firebaseApp);
-      const id = getAuth().currentUser.uid
-      const applicationName = x.companyname.concat(" - ", x.jobpos, " - ", id)
-      const jobName = x.companyname.concat(" - ", x.jobpos)
-      const docRef = doc(db, "Application", applicationName)
+      const id = getAuth().currentUser.uid;
+      const applicationName = x.companyname.concat(" - ", x.jobpos, " - ", id);
+      const jobName = x.companyname.concat(" - ", x.jobpos);
+      const docRef = doc(db, "Application", applicationName);
       const docSnap = await getDoc(docRef);
       const docRef1 = doc(db, "Job", jobName);
       const docRef2 = doc(db, "User", id);
 
       if (docSnap.exists()) {
-        alert("You have already applied for this position")
+        alert("You have already applied for this position");
       } else {
-      try {
-        //Add document into application db
+        try {
+          //Add document into application db
 
-        const data = {
-          CreatedAt : serverTimestamp(),
-          Progress : "Pending",
-          Applicant : id,
-          Position : x.jobpos,
-          Status : "",
-          CompanyName : x.companyname
+          const data = {
+            CreatedAt: serverTimestamp(),
+            Progress: "Pending",
+            Applicant: id,
+            Position: x.jobpos,
+            Status: "",
+            CompanyName: x.companyname,
+          };
+
+          await setDoc(docRef, data);
+          await updateDoc(docRef1, { Applicants: arrayUnion(id) });
+          await updateDoc(docRef2, {
+            JobsApplied: arrayUnion(applicationName),
+          });
+          alert("Job applied!");
+        } catch (error) {
+          alert("There was an error processing the application");
+          console.log(error);
         }
-
-        await setDoc(docRef, data)
-        await updateDoc(docRef1, {Applicants : arrayUnion(id)});
-        await updateDoc(docRef2, {JobsApplied : arrayUnion(applicationName)});
-        alert("Job applied!")
-      } catch (error) {
-        alert("There was an error processing the application")
-        console.log(error)
       }
-      }
-    }
+    },
   },
 
   async beforeMount() {
     async function getData() {
       try {
-      const db = getFirestore(firebaseApp);
-      const querySnapshot =  await getDocs(collection(db, "Job"));
-      querySnapshot.forEach((doc) => 
-      jobData.push({
-      companyname: doc.data().CompanyName,
-      jobpos: doc.data().InternshipTitle,
-      postdate: doc.data().CreatedAt.toDate().toString().slice(4,15),
-      duration: doc.data().Duration,
-      yos: doc.data().Year,
-      range: doc.data().DateRange[0].toDate().toString().slice(4,15)
-      .concat(" - ", doc.data().DateRange[1].toDate().toString().slice(4,15))}))
-      console.log("success")
+        const db = getFirestore(firebaseApp);
+        const querySnapshot = await getDocs(collection(db, "Job"));
+        querySnapshot.forEach((doc) =>
+          jobData.push({
+            companyname: doc.data().CompanyName,
+            jobpos: doc.data().InternshipTitle,
+            postdate: doc.data().CreatedAt.toDate().toString().slice(4, 15),
+            duration: doc.data().Duration,
+            yos: doc.data().Year,
+            range: doc
+              .data()
+              .DateRange[0].toDate()
+              .toString()
+              .slice(4, 15)
+              .concat(
+                " - ",
+                doc.data().DateRange[1].toDate().toString().slice(4, 15)
+              ),
+          })
+        );
+        console.log("success");
       } catch (error) {
         console.error(error);
       }
     }
     await getData();
-    console.log("hello")
+    console.log("hello");
   },
-}
-  // setup() {
+};
+// setup() {
 
-  //   const searchQuery = ref("");
+//   const searchQuery = ref("");
 
-  //   computed: {
-  //     searchResult() {
-  //       const {jobData, keyword} = this;
-  //       return jobData.filter(({companyname}) => companyname.includes(keyword));
-  //     },
-  //   },
+//   computed: {
+//     searchResult() {
+//       const {jobData, keyword} = this;
+//       return jobData.filter(({companyname}) => companyname.includes(keyword));
+//     },
+//   },
 
-  // const searchResult = computed(() => {
-  //   return jobData.value.filter(
-  //     (data) =>
-  //       !searchQuery.value ||
-  //       data.name.toLowerCase().includes(searchQuery.value.toLowerCase())
-  //     );
-  //   });
+// const searchResult = computed(() => {
+//   return jobData.value.filter(
+//     (data) =>
+//       !searchQuery.value ||
+//       data.name.toLowerCase().includes(searchQuery.value.toLowerCase())
+//     );
+//   });
 
-  //       return {searchResult, searchQuery};
-  //     },
-
+//       return {searchResult, searchQuery};
+//     },
 </script>
 
 <style scoped>
