de15ae17b197fdc9a2f4fb912bf11f4a78615c5b
lohdaijiu <lohdaijiu@gmail.com>
2022-03-30 10:59:45 +0800

    Show chat

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -1,14 +1,18 @@
 <template>
   <div class="wrapper">
     <section>
-      <main>
-        <div v-for="(msg, index) in messages" v-bind:key="'index-'+index"
-             :class="['message', sentOrReceived(msg.userUID)]">
-          <p>{{ msg.text }}</p>
-        </div>
 
-        <div ref="scrollable"></div>
-      </main>
+	<el-table
+    :data="messages"
+    style="width: 100%"
+    :row-class-name="tableRowClassName"
+    align = "center"
+    margin-top = "100px"
+  >
+    <el-table-column prop="sender"  width="180" />
+    <el-table-column prop="receiver" width="180" align= "right"/>
+
+  </el-table>
 
       <form v-on:submit.prevent="sendMessage">
         <input v-model="message" type="text" placeholder="Enter your message!">
@@ -22,18 +26,36 @@
 import firebaseApp from "../main.js";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
-import { collection, addDoc, serverTimestamp, onSnapshot, orderBy, query } from "firebase/firestore";
+import { collection, addDoc, serverTimestamp, onSnapshot, orderBy, query, doc, getDoc } from "firebase/firestore";
 
 
 export default {
-  mounted() {
+  async mounted() {
 	try{
     const db = getFirestore(firebaseApp);
 	const id = getAuth().currentUser.uid;
-	const q = query(collection(db, id.concat(' - ', this.$route.query.id)), orderBy("createdAt"));
+
+	const docRef = doc(db, "User", id)
+	const docSnap = await getDoc(docRef)
+	this.employer = docSnap.data().Employer
+
+	if (this.employer) {
+		this.collectionName = id.concat(' - ', this.$route.query.id)
+	} else {
+		this.collectionName = this.$route.query.id.concat(' - ', id)
+	}
+	console.log(this.collectionName)
+	const q = query(collection(db, this.collectionName), orderBy("createdAt"));
 	onSnapshot(q, (querySnapshot) => {
-		this.messages = querySnapshot.docs.map(doc => doc.data())
+		this.messages = querySnapshot.docs.map(doc => {
+			if (doc.data().userUID == id) {
+				return {sender: "", receiver: doc.data().text}
+			} else {
+				return {sender: doc.data().text, receiver: ""}
+			}
+		})
 	})
+	console.log(this.messages)
 	} catch (error) {
 		console.log(error)
 	}
@@ -41,8 +63,10 @@ export default {
   },
   data() {
     return {
-      message: '',
-      messages: [],
+		message: '',
+		messages: [],
+		employer : false,
+		collectionName : ""
     }
   },
   methods: {
@@ -59,7 +83,7 @@ export default {
         'text': this.message,
         'createdAt': serverTimestamp(),
       }
-      await addDoc(collection(db, id.concat(' - ', this.$route.query.id)), messageInfo)
+      await addDoc(collection(db, this.collectionName), messageInfo)
       this.message = ''
       this.$refs['scrollable'].scrollIntoView({ behavior: 'smooth' })
     }
@@ -68,209 +92,5 @@ export default {
 </script>
 
 <style scoped>
-* {
-	font-family: Avenir, Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	margin: 0;
-	padding: 0;
-	box-sizing: border-box;
-}
-.view {
-	display: flex;
-	justify-content: center;
-	min-height: 100vh;
-	background-color: #ea526f;
-	
-	&.login {
-		align-items: center;
-		.login-form {
-			display: block;
-			width: 100%;
-			padding: 15px;
-			
-			.form-inner {
-				display: block;
-				background-color: #FFF;
-				padding: 50px 15px;
-				border-radius: 16px;
-				box-shadow: 0px 6px 12px rgba(0, 0, 0, 0.2);
-				h1 {
-					color: #AAA;
-					font-size: 28px;
-					margin-bottom: 30px;
-				}
-				label {
-					display: block;
-					margin-bottom: 5px;
-					color: #AAA;
-					font-size: 16px;
-					transition: 0.4s;
-				}
-				input[type="text"] {
-					appearance: none;
-					border: none;
-					outline: none;
-					background: none;
-					display: block;
-					width: 100%;
-					padding: 10px 15px;
-					border-radius: 8px;
-					margin-bottom: 15px;
-					
-					color: #333;
-					font-size: 18px;
-					box-shadow: 0px 0px 0px rgba(0, 0, 0, 0);
-					background-color: #F3F3F3;
-					transition: 0.4s;
-					&::placeholder {
-						color: #888;
-						transition: 0.4s;
-					}
-				}
-				input[type="submit"] {
-					appearance: none;
-					border: none;
-					outline: none;
-					background: none;
-					display: block;
-					width: 100%;
-					padding: 10px 15px;
-					background-color: #ea526f;
-					border-radius: 8px;
-					color: #FFF;
-					font-size: 18px;
-					font-weight: 700;
-				}
-				&:focus-within {
-					label {
-						color: #ea526f;
-					}
-					input[type="text"] {
-						background-color: #FFF;
-						box-shadow: 0 0 6px rgba(0, 0, 0, 0.2);
-						&::placeholder {
-							color: #666;
-						}
-					}
-				}
-			}
-		}
-	}
-	&.chat {
-		flex-direction: column;
-		header {
-			position: relative;
-			display: block;
-			width: 100%;
-			padding: 50px 30px 10px;
-			.logout {
-				position: absolute;
-				top: 15px;
-				right: 15px;
-				appearance: none;
-				border: none;
-				outline: none;
-				background: none;
-				
-				color: #FFF;
-				font-size: 18px;
-				margin-bottom: 10px;
-				text-align: right;
-			}
-			h1 {
-				color: #FFF;
-			}
-		}
-		.chat-box {
-			border-radius: 24px 24px 0px 0px;
-			background-color: #FFF;
-			box-shadow: 0px 0px 12px rgba(100, 100, 100, 0.2);
-			flex: 1 1 100%;
-			padding: 30px;
-			.message {
-				display: flex;
-				margin-bottom: 15px;
-				
-				.message-inner {
-					.username {
-						color: #888;
-						font-size: 16px;
-						margin-bottom: 5px;
-						padding-left: 15px;
-						padding-right: 15px;
-					}
-					.content {
-						display: inline-block;
-						padding: 10px 20px;
-						background-color: #F3F3F3;
-						border-radius: 999px;
-						color: #333;
-						font-size: 18px;
-						line-height: 1.2em;
-						text-align: left;
-					}
-				}
-				&.current-user {
-					margin-top: 30px;
-					justify-content: flex-end;
-					text-align: right;
-					.message-inner {
-						max-width: 75%;
-						.content {
-							color: #FFF;
-							font-weight: 600;
-							background-color: #ea526f;
-						}
-					}
-				}
-			}
-		}
-		footer {
-			position: sticky;
-			bottom: 0px;
-			background-color: #FFF;
-			padding: 30px;
-			box-shadow: 0px 0px 12px rgba(100, 100, 100, 0.2);
-			form {
-				display: flex;
-				input[type="text"] {
-					flex: 1 1 100%;
-					appearance: none;
-					border: none;
-					outline: none;
-					background: none;
-					display: block;
-					width: 100%;
-					padding: 10px 15px;
-					border-radius: 8px 0px 0px 8px;
-					
-					color: #333;
-					font-size: 18px;
-					box-shadow: 0px 0px 0px rgba(0, 0, 0, 0);
-					background-color: #F3F3F3;
-					transition: 0.4s;
-					&::placeholder {
-						color: #888;
-						transition: 0.4s;
-					}
-				}
-				
-				input[type="submit"] {
-					appearance: none;
-					border: none;
-					outline: none;
-					background: none;
-					display: block;
-					padding: 10px 15px;
-					border-radius: 0px 8px 8px 0px;
-					background-color: #ea526f;
-					color: #FFF;
-					font-size: 18px;
-					font-weight: 700;
-				}
-			}
-		}
-	}
-}
+
 </style>
